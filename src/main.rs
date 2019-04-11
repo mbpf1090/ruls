8dd0e7165001c896c4aedc0a8142047d72b00586
Sebastian Weidele <basti@Sebastians-MacBook-Pro-2.local>
2019-04-11 05:47:20 +0200

    added basic loop

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
-use std::fs;
 use std::env;
 use std::path;
-use std::fs::DirEntry;
+use std::fs::{DirEntry, ReadDir};
 
 fn main() {
     let mut args = env::args();
     args.next();
 
-    let path_str = match args.next() { //String
+    let path_str: String = match args.next() {
         Some(path) => path,
         None => String::from("Error")
     };
     
-    let path = path::Path::new(&path_str); // Path
+    let path: &path::Path = path::Path::new(&path_str);
     println!("{}", path.display());
-    let result = path.read_dir().unwrap();           //Iterator
+    let result = path.read_dir().expect("Error getting directory");
     for item in result {
-        //println!("{}", item.unwrap().path().to_str().unwrap());
-        println!("{:?}", item.unwrap().path());
+        let item = item.unwrap();
+        
+        if item.metadata().unwrap().is_dir() {
+            println!("{}", item.file_name().into_string().unwrap());
+        } else {
+            println!("{:?}", item.file_type());
+        }
     }
 }
+
