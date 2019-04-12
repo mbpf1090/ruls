d34b82777abda2c42248ced5f68fb17726cbc6a1
Sebastian Weidele <basti@Sebastians-MacBook-Pro-2.local>
2019-04-12 18:45:32 +0200

    added permissions in reversed order

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::env;
 use std::path;
 use std::fs::{DirEntry, ReadDir};
 use colored::*;
+use std::os::unix::fs::PermissionsExt;
 
 fn main() {
     let mut args = env::args();
@@ -16,15 +17,45 @@ fn main() {
     
     let path: &path::Path = path::Path::new(&path_str);
     println!("{}", path.display());
-    let result = path.read_dir().expect("Error getting directory");
-    for item in result {
+    let paths = path.read_dir().expect("Error getting directory");
+    for item in paths {
         let item = item.unwrap();
         
         if item.metadata().unwrap().is_dir() {
-            println!("{}", item.file_name().into_string().unwrap().green());
+            println!("{} -> {}",mode_to_perm_str(&item), item.file_name().into_string().unwrap().green());
         } else {
-            println!("{}", item.file_name().into_string().unwrap().yellow());
+            println!("{} -> {}",mode_to_perm_str(&item), item.file_name().into_string().unwrap().yellow());
         }
     }
 }
 
+fn mode_to_perm_str(entry: &DirEntry) -> String {
+    let mut s = String::new();
+    let mut mode = entry.metadata().unwrap().permissions().mode();
+    mode = mode & 0b0000000_111_111_111;
+    for i in 0..3 {
+        if i == 0 {
+            mode = mode >> 0;
+        } else {
+            mode = mode >> 3;
+        }
+        let foo = mode & 0b111;
+        if foo & 0b100 == 0 {
+            s.push('-')
+        } else {
+            s.push('r')
+        }
+        if foo & 0b010 == 0 {
+            s.push('-')
+        } else {
+            s.push('w')
+        }
+        if foo & 0b001 == 0 {
+            s.push('-')
+        } else {
+            s.push('x')
+        }
+        s.push(' ');
+    }
+    s
+}
